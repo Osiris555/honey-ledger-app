85ca27e3e768f27b3b9b406232b0026c7fda42a1
Derek V <ath3nian17@proton,me>
2026-01-03 23:10:06 -0400

    Ledger GET_PUBLIC_KEY APDU

--- src/apdu.c
+++ src/apdu.c
@@ -1,61 +1,117 @@
 #include "apdu.h"
-#include "os_io_seproxyhal.h"
 #include <string.h>
 
-/* CLA */
-#define HONEY_CLA 0xE0
+#include "os.h"
+#include "cx.h"
 
-/* INS codes */
-#define INS_GET_APP_VERSION 0x01
-#define INS_GET_APP_NAME    0x02
+// APDU offsets
+#define OFFSET_CLA   0
+#define OFFSET_INS   1
 
-/* App metadata */
-#define APP_NAME    "Honey Ledger"
-#define APP_VERSION "0.2.0"
+// Status words
+#define SW_OK                0x9000
+#define SW_INS_NOT_SUPPORTED 0x6D00
+#define SW_CLA_NOT_SUPPORTED 0x6E00
+#define SW_EXECUTION_FAILED  0x6400
 
-/* Global APDU buffer */
-extern unsigned char G_io_apdu_buffer[IO_APDU_BUFFER_SIZE];
+// Hardened index macro
+#define HARDENED 0x80000000
 
-/* Handle APDU */
-void handle_apdu(uint8_t *apdu_buffer, uint16_t apdu_length) {
-    uint8_t cla = apdu_buffer[OFFSET_CLA];
-    uint8_t ins = apdu_buffer[OFFSET_INS];
+static uint16_t handle_get_public_key(uint8_t *buffer) {
+    cx_ecfp_private_key_t privkey;
+    cx_ecfp_public_key_t pubkey;
 
-    /* Enforce correct CLA */
-    if (cla != HONEY_CLA) {
-        G_io_apdu_buffer[0] = 0x6E;
-        G_io_apdu_buffer[1] = 0x00;
-        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
+    uint8_t pubkey_raw[65];
+
+    // BIP32 path: m/44'/9999'/0'/0/0
+    uint32_t path[] = {
+        44  | HARDENED,
+        9999 | HARDENED,
+        0   | HARDENED,
+        0,
+        0
+    };
+
+    // Derive private key from Ledger seed
+    cx_err_t err = os_perso_derive_node_bip32(
+        CX_CURVE_256K1,
+        path,
+        5,
+        privkey.d,
+        NULL
+    );
+
+    if (err != CX_OK) {
+        return 0;
+    }
+
+    // Initialize private key structure
+    cx_ecfp_init_private_key(
+        CX_CURVE_256K1,
+        privkey.d,
+        32,
+        &privkey
+    );
+
+    // Generate corresponding public key
+    err = cx_ecfp_generate_pair(
+        CX_CURVE_256K1,
+        &pubkey,
+        &privkey,
+        1
+    );
+
+    if (err != CX_OK) {
+        return 0;
+    }
+
+    // Export uncompressed public key
+    err = cx_ecfp_export_public_key(&pubkey, pubkey_raw, sizeof(pubkey_raw));
+    if (err != CX_OK) {
+        return 0;
+    }
+
+    memcpy(buffer, pubkey_raw, sizeof(pubkey_raw));
+
+    // Wipe private key
+    explicit_bzero(&privkey, sizeof(privkey));
+
+    return sizeof(pubkey_raw);
+}
+
+void handle_apdu(uint8_t *buffer, uint16_t length) {
+    uint16_t tx = 0;
+
+    uint8_t cla = buffer[OFFSET_CLA];
+    uint8_t ins = buffer[OFFSET_INS];
+
+    if (cla != CLA_HONEY) {
+        buffer[tx++] = (SW_CLA_NOT_SUPPORTED >> 8) & 0xFF;
+        buffer[tx++] = SW_CLA_NOT_SUPPORTED & 0xFF;
+        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, tx);
         return;
     }
 
     switch (ins) {
-
-        /* GET_APP_VERSION */
-        case INS_GET_APP_VERSION: {
-            size_t len = strlen(APP_VERSION);
-            memcpy(G_io_apdu_buffer, APP_VERSION, len);
-            G_io_apdu_buffer[len]     = 0x90;
-            G_io_apdu_buffer[len + 1] = 0x00;
-            io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, len + 2);
+        case INS_GET_PUBLIC_KEY:
+            tx = handle_get_public_key(buffer);
+            if (tx == 0) {
+                buffer[0] = (SW_EXECUTION_FAILED >> 8) & 0xFF;
+                buffer[1] = SW_EXECUTION_FAILED & 0xFF;
+                io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
+                return;
+            }
             break;
-        }
 
-        /* GET_APP_NAME */
-        case INS_GET_APP_NAME: {
-            size_t len = strlen(APP_NAME);
-            memcpy(G_io_apdu_buffer, APP_NAME, len);
-            G_io_apdu_buffer[len]     = 0x90;
-            G_io_apdu_buffer[len + 1] = 0x00;
-            io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, len + 2);
-            break;
-        }
-
-        /* Unknown INS */
         default:
-            G_io_apdu_buffer[0] = 0x6D;
-            G_io_apdu_buffer[1] = 0x00;
-            io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
-            break;
+            buffer[tx++] = (SW_INS_NOT_SUPPORTED >> 8) & 0xFF;
+            buffer[tx++] = SW_INS_NOT_SUPPORTED & 0xFF;
+            io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, tx);
+            return;
     }
+
+    buffer[tx++] = (SW_OK >> 8) & 0xFF;
+    buffer[tx++] = SW_OK & 0xFF;
+
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, tx);
 }
