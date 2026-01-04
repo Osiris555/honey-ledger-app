e59025c445c2718e6b7c4516eb2c3c66e94e3be7
Derek V <ath3nian17@proton,me>
2026-01-03 22:39:17 -0400

    Add GET_APP_VERSION APDU

--- src/main.c
+++ src/main.c
@@ -1,67 +1,61 @@
-#include <os.h>
-#include <cx.h>
-#include <string.h>
+#include "os.h"
+#include "cx.h"
+#include "ux.h"
+#include "os_io_seproxyhal.h"
 
-#include "apdu.h"
+#define CLA_HONEY 0xE0
+#define INS_GET_VERSION 0x01
 
 static uint8_t G_io_apdu_buffer[IO_APDU_BUFFER_SIZE];
 
-void app_main(void) {
-    for (;;) {
-        // Receive APDU
-        unsigned int rx = io_exchange(CHANNEL_APDU, 0);
+void handle_get_version(void) {
+    G_io_apdu_buffer[0] = 0x01; // major
+    G_io_apdu_buffer[1] = 0x00; // minor
+    G_io_apdu_buffer[2] = 0x00; // patch
+    io_send_response_buffers(G_io_apdu_buffer, 3, SW_OK);
+}
 
-        if (rx < 5) {
-            continue;
-        }
+void handle_apdu(uint8_t cla, uint8_t ins) {
+    if (cla != CLA_HONEY) {
+        THROW(SW_CLA_NOT_SUPPORTED);
+    }
 
-        uint8_t cla = G_io_apdu_buffer[0];
-        uint8_t ins = G_io_apdu_buffer[1];
-        uint8_t p1  = G_io_apdu_buffer[2];
-        uint8_t p2  = G_io_apdu_buffer[3];
-        uint8_t lc  = G_io_apdu_buffer[4];
-
-        const uint8_t *data = &G_io_apdu_buffer[5];
-        uint8_t *tx = G_io_apdu_buffer;
-
-        size_t tx_len = 0;
-        uint16_t sw = 0x9000;
-
-        handle_apdu(
-            cla,
-            ins,
-            p1,
-            p2,
-            lc,
-            data,
-            tx,
-            &tx_len,
-            &sw
-        );
-
-        // Append status word
-        tx[tx_len++] = (sw >> 8) & 0xFF;
-        tx[tx_len++] = sw & 0xFF;
-
-        io_exchange(CHANNEL_APDU, tx_len);
+    switch (ins) {
+        case INS_GET_VERSION:
+            handle_get_version();
+            break;
+        default:
+            THROW(SW_INS_NOT_SUPPORTED);
     }
 }
 
 __attribute__((noreturn))
-void main(void) {
-    os_boot();
-
+void app_main(void) {
     for (;;) {
+        unsigned int rx = io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 0);
+        if (rx == 0) continue;
+
+        uint8_t cla = G_io_apdu_buffer[0];
+        uint8_t ins = G_io_apdu_buffer[1];
+
         BEGIN_TRY {
             TRY {
-                app_main();
+                handle_apdu(cla, ins);
             }
             CATCH_OTHER(e) {
-                // ignore
+                G_io_apdu_buffer[0] = (e >> 8) & 0xFF;
+                G_io_apdu_buffer[1] = e & 0xFF;
+                io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
             }
             FINALLY {
             }
         }
         END_TRY;
     }
+}
+
+__attribute__((noreturn))
+void main(void) {
+    os_boot();
+    app_main();
 }
